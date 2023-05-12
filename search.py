a42f953ba43b260fc2e9663327c9ea38aa3cf343
bahy99 <bahyhassan2@gmail.com>
2023-05-12 19:30:03 +0300

    search the projects using different keys model

---
+++ search.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+def search_projects(user):
+    c = int(
+        input("Choose the way you want to search:\n1. By title\n2. By details\n3. By total target\n4. By start date\n"))
+
+    if c == 1:
+        t = input("Enter the title: ")
+        with open("projects.txt", "r") as file:
+            lines = file.readlines()
+            for project_line in lines:
+                data = project_line.split(":")
+                if t == data[1]:
+                    print("Title:", data[2])
+                    print("Details:", data[2])
+                    print("Total Target:", data[3])
+                    print("Start Time:", data[4])
+                    print("End Time:", data[5])
+
+    elif c == 2:
+        d = input("Enter the details: ")
+        with open("projects.txt", "r") as file:
+            lines = file.readlines()
+            for project_line in lines:
+                data = project_line.split(":")
+                if d == data[2]:
+                    print("Title:", data[2])
+                    print("Details:", data[2])
+                    print("Total Target:", data[3])
+                    print("Start Time:", data[4])
+                    print("End Time:", data[5])
+
+
+search_projects("user")
